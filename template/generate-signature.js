9389d0ba85ab839934ad7d2b785d4d3550aafd50
TEC-EA <tec.ea.hub@gmail.com>
2025-11-05 13:15:15 +0000

    Create generate-signature.js

---
+++ template/generate-signature.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+const [firstName, lastName, jobTitle] = process.argv.slice(2);
+const folderName = `${firstName.toLowerCase()}-${lastName.toLowerCase()}`;
+const outputDir = path.join('employee', folderName);
+
+// Read template
+const template = fs.readFileSync('template/signature-template.html', 'utf8');
+
+// Replace placeholders
+const html = template
+  .replace('{{FULL_NAME}}', `${firstName} ${lastName}`)
+  .replace('{{JOB_TITLE}}', jobTitle);
+
+// Create employee folder if it doesn't exist
+fs.mkdirSync(outputDir, { recursive: true });
+
+// Write the generated signature
+fs.writeFileSync(path.join(outputDir, 'index.html'), html);
+
+console.log(`Signature created for ${firstName} ${lastName} at ${outputDir}`);
