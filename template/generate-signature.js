6d2d649c004618ee74c6fcb08967dfeef7ba1e7b
TEC-EA <tec.ea.hub@gmail.com>
2025-11-05 13:22:45 +0000

    Update generate-signature.js

--- template/generate-signature.js
+++ template/generate-signature.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
 const [firstName, lastName, jobTitle] = process.argv.slice(2);
 const folderName = `${firstName.toLowerCase()}-${lastName.toLowerCase()}`;
